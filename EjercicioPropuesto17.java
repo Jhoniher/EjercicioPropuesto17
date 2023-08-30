2d2148a50daabef3a8e233b9088aebebc4a6a384
Jhony Alpala <jhoniher95@gmail.com>
2023-08-30 17:24:53 -0500

    Ejercicio finalizado

--- EjercicioPropuesto17.java
+++ EjercicioPropuesto17.java
@@ -9,9 +9,9 @@ public class EjercicioPropuesto17 {
         Scanner entrada=new Scanner(System.in);
         double radio, area, circunferencia;
         System.out.println("Ingrese el radio del circulo: ");
-        radio=entrada.nextDouble();
-        area=Math.PI*Math.pow(radio,2);
-        circunferencia=2*Math.PI*radio;
+        radio=entrada.nextDouble(); //Se ingresa el radio del circulo
+        area=Math.PI*Math.pow(radio,2); //Se calcula el area
+        circunferencia=2*Math.PI*radio; //Se calcula la circunferencia
         System.out.println("El area del circulo es: "+area);
         System.out.println("La longitud de la circunferencia es: "+circunferencia);
     }
