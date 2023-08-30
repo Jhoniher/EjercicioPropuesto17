b9b740cd2b4e7a72c4d0ac38b9343fcb0227baa6
Jhony Alpala <jhoniher95@gmail.com>
2023-08-30 17:21:59 -0500

    Operaciones realizadas

--- EjercicioPropuesto17.java
+++ EjercicioPropuesto17.java
@@ -1,10 +1,17 @@
 
 package ejerciciopropuesto17;
 
+import java.util.Scanner;
+
 public class EjercicioPropuesto17 {
 
     public static void main(String[] args) {
-        
+        Scanner entrada=new Scanner(System.in);
+        double radio, area, circunferencia;
+        System.out.println("Ingrese el radio del circulo: ");
+        radio=entrada.nextDouble();
+        area=Math.PI*Math.pow(radio,2);
+        circunferencia=2*Math.PI*radio;
     }
     
 }
